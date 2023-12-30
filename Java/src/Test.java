3f4e7e75f996fabad80e11b43a6e8a19dea7a4de
Archit Sharma <42913822+archit-sharma1@users.noreply.github.com>
2023-12-31 04:34:09 +0530

    Neet code practice 31 Dec 2023
    Added following codes:
        1. Product Except self
        2. Encode decode string
    Added following directories:
        1. Editorial answers
        2. Video solutions

--- Java/src/Test.java
+++ Java/src/Test.java
@@ -2,9 +2,11 @@ import Algorithms.QuickSort;
 import General.FreqHashMap;
 import General.Patterns;
 import NeetCodePractice.AnagramCheck;
+import NeetCodePractice.EncodeDecodeStrings;
 import NeetCodePractice.GroupAnagrams;
 //import NeetCodePractice.TopKFrequentElements;
 import NeetCodePractice.EditorialAnswers.TopKFrequentElements;
+import NeetCodePractice.ProductExceptSelf;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -12,8 +14,29 @@ import java.util.List;
 
 public class Test{
     // Driver program to test above function
+    public static void testTopKFrequentElements(){
+        System.out.println(Arrays.toString(new TopKFrequentElements().topKFrequent(new int[]{4,1,-1,2,-1,2,3},2)));
+    }
+
+    public static void testProductExceptSelf(){
+        System.out.println(Arrays.toString(new int[]{1,2,3,4}));
+        System.out.println(Arrays.toString(new ProductExceptSelf().productExceptSelf(new int[]{1})));
+    }
+
+    public static void testEncodeDecodeStrings(){
+        String encodedMessage = new EncodeDecodeStrings().encode(Arrays.asList("lint","code","love","you"));
+        System.out.println(new EncodeDecodeStrings().decode(encodedMessage));
+    }
+
+    public static void testVideoSolutionEncodeDecodeStrings(){
+        String encodedMessage = new NeetCodePractice.VideoSolution.EncodeDecodeStrings().encode(Arrays.asList("lint","code","love","you"));
+        System.out.println(encodedMessage);
+        System.out.println(new NeetCodePractice.VideoSolution.EncodeDecodeStrings().decode(encodedMessage));
+    }
+
     public static void main(String[] args) {
-//        System.out.println(new FreqHashMap().freqMap(new int[]{1,1,1,2,2,3})); ;
-            System.out.println(Arrays.toString(new TopKFrequentElements().topKFrequent(new int[]{4,1,-1,2,-1,2,3},2)));
+//        String str = "4;lint4;code4;love3;you";
+//        System.out.println(str.substring(0,1));
+        testVideoSolutionEncodeDecodeStrings();
     }
 }
