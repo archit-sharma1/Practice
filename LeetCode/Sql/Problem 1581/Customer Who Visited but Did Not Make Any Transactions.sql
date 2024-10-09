17d44d812d39796728848c96b89ceb206ff0c1a0
Archit Sharma <42913822+archit-sharma1@users.noreply.github.com>
2024-10-10 01:06:15 +0530

    Update Customer Who Visited but Did Not Make Any Transactions.sql
    Solution Leet code 1581 sql problem statement

--- LeetCode/Sql/Problem 1581/Customer Who Visited but Did Not Make Any Transactions.sql
+++ LeetCode/Sql/Problem 1581/Customer Who Visited but Did Not Make Any Transactions.sql
@@ -1,3 +1,4 @@
+--Method -1
 select
 customer_id, count(visit_id) as count_no_trans
 from visits
@@ -8,3 +9,12 @@ where not exists (
     where visits.visit_id = transactions.visit_id
 )
 group by customer_id
+
+--Method 2
+
+select 
+customer_id, count(*) as [count_no_trans]
+from Visits
+left join Transactions on Visits.visit_id = Transactions.visit_id
+WHERE Transactions.visit_id is null
+GROUP BY (customer_id);
