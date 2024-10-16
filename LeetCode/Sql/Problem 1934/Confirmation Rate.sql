af9872f7e85cc73afebffc0114efcba12e62800e
Archit Sharma <42913822+archit-sharma1@users.noreply.github.com>
2024-10-16 17:49:46 +0530

    Create Confirmation Rate.sql
    Solution to Leet code 1934 sql problem statement

---
+++ LeetCode/Sql/Problem 1934/Confirmation Rate.sql
@@ -0,0 +1,24 @@
+select
+a.user_id, round(isnull(cast(b.total_actions as float)/cast(nullif(a.total_actions, 0) as float),0.00),2)  as confirmation_rate
+-- *
+from
+(select 
+Signups.user_id,count(action) as total_actions
+from Signups
+left join Confirmations
+on Signups.user_id = Confirmations.user_id
+-- where action <> 'timeout' or action is null 
+group by Signups.user_id
+-- having count(action) >= 0 
+) a
+left join (
+select 
+Signups.user_id,count(action) as total_actions
+from Signups
+left join Confirmations
+on Signups.user_id = Confirmations.user_id
+where action <> 'timeout' or action is null 
+group by Signups.user_id
+having count(action) >= 0 ) b
+on a.user_id = b.user_id
+
